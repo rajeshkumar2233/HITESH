642864465c0bac1fa6dea6bcea76f03649d06f4f
rajeshmajhi2233 <rajeshmajhi2233@gmail.com>
2024-03-04 20:06:06 +0530

    utils code

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,14 @@ dotenv.config({
 })
  
 connectDB()
-
+.then(()=>{
+    app.listen(process.env.PORT || 5000, ()=>{
+        console.log(`server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("mongodb connection failed or ERROR:",err)
+})
 
 
 
